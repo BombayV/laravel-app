08c581862d9803f00b1bbb5eda36aff1391d020e
BombayV <OMGmsebastian@gmail.com>
2024-07-02 22:51:51 -0600

    fix: only active users

--- app/Http/Controllers/Dashboard/ClientSearch.php
+++ app/Http/Controllers/Dashboard/ClientSearch.php
@@ -16,9 +16,13 @@ class ClientSearch extends Controller
       return $this->sendError('No se ha ingresado un valor de búsqueda');
     }
 
-    $result = Cliente::where('cli_nom', 'like', "%$id%")
-      ->orWhere('cli_ape', 'like', "%$id%")
-      ->orWhere('cli_ema', 'like', "%$id%")
+    // Cli_nom, cli_ape, cli_ema and cli_est is not 0
+    $result = Cliente::where('cli_est', '>', 0)
+      ->where(function ($query) use ($id) {
+        $query->where('cli_nom', 'like', '%' . $id . '%')
+          ->orWhere('cli_ape', 'like', '%' . $id . '%')
+          ->orWhere('cli_ema', 'like', '%' . $id . '%');
+      })
       ->get();
 
     return $this->sendResponse($result, 'Clientes encontrados');
